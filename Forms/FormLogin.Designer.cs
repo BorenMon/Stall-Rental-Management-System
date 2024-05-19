84b8086ffcbbdf78863c9f27aeed79394c333cde
Mon Boren <63392246+BorenMon@users.noreply.github.com>
2024-05-19 20:13:27 +0700

    Remove Krypton Package

--- Forms/FormLogin.Designer.cs
+++ Forms/FormLogin.Designer.cs
@@ -28,166 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.kryptonTextBoxPhoneNumber = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
-            this.labelPhoneNumber = new System.Windows.Forms.Label();
-            this.labelLogin = new System.Windows.Forms.Label();
-            this.labelPassword = new System.Windows.Forms.Label();
-            this.kryptonTextBoxPassword = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
-            this.labelUserType = new System.Windows.Forms.Label();
-            this.kryptonComboBox1 = new ComponentFactory.Krypton.Toolkit.KryptonComboBox();
-            this.kryptonButtonLogin = new ComponentFactory.Krypton.Toolkit.KryptonButton();
-            ((System.ComponentModel.ISupportInitialize)(this.kryptonComboBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // kryptonTextBoxPhoneNumber
-            // 
-            this.kryptonTextBoxPhoneNumber.Location = new System.Drawing.Point(269, 269);
-            this.kryptonTextBoxPhoneNumber.Name = "kryptonTextBoxPhoneNumber";
-            this.kryptonTextBoxPhoneNumber.Size = new System.Drawing.Size(542, 70);
-            this.kryptonTextBoxPhoneNumber.StateCommon.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(92)))), ((int)(((byte)(80)))));
-            this.kryptonTextBoxPhoneNumber.StateCommon.Border.Color2 = System.Drawing.Color.Transparent;
-            this.kryptonTextBoxPhoneNumber.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
-            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
-            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
-            this.kryptonTextBoxPhoneNumber.StateCommon.Border.Rounding = 8;
-            this.kryptonTextBoxPhoneNumber.StateCommon.Border.Width = 2;
-            this.kryptonTextBoxPhoneNumber.StateCommon.Content.Font = new System.Drawing.Font("Poppins Medium", 12F);
-            this.kryptonTextBoxPhoneNumber.StateCommon.Content.Padding = new System.Windows.Forms.Padding(15);
-            this.kryptonTextBoxPhoneNumber.TabIndex = 0;
-            // 
-            // labelPhoneNumber
-            // 
-            this.labelPhoneNumber.AutoSize = true;
-            this.labelPhoneNumber.Font = new System.Drawing.Font("Poppins Medium", 12F);
-            this.labelPhoneNumber.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(114)))), ((int)(((byte)(114)))));
-            this.labelPhoneNumber.Location = new System.Drawing.Point(263, 220);
-            this.labelPhoneNumber.Name = "labelPhoneNumber";
-            this.labelPhoneNumber.Size = new System.Drawing.Size(165, 36);
-            this.labelPhoneNumber.TabIndex = 1;
-            this.labelPhoneNumber.Text = "Phone Number";
-            // 
-            // labelLogin
-            // 
-            this.labelLogin.AutoSize = true;
-            this.labelLogin.Font = new System.Drawing.Font("Poppins", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(114)))), ((int)(((byte)(114)))));
-            this.labelLogin.Location = new System.Drawing.Point(542, 67);
-            this.labelLogin.Name = "labelLogin";
-            this.labelLogin.Size = new System.Drawing.Size(127, 58);
-            this.labelLogin.TabIndex = 2;
-            this.labelLogin.Text = "LOGIN";
-            // 
-            // labelPassword
-            // 
-            this.labelPassword.AutoSize = true;
-            this.labelPassword.Font = new System.Drawing.Font("Poppins Medium", 12F);
-            this.labelPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(114)))), ((int)(((byte)(114)))));
-            this.labelPassword.Location = new System.Drawing.Point(263, 382);
-            this.labelPassword.Name = "labelPassword";
-            this.labelPassword.Size = new System.Drawing.Size(115, 36);
-            this.labelPassword.TabIndex = 4;
-            this.labelPassword.Text = "Password";
-            // 
-            // kryptonTextBoxPassword
-            // 
-            this.kryptonTextBoxPassword.Location = new System.Drawing.Point(269, 431);
-            this.kryptonTextBoxPassword.Name = "kryptonTextBoxPassword";
-            this.kryptonTextBoxPassword.Size = new System.Drawing.Size(542, 70);
-            this.kryptonTextBoxPassword.StateCommon.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(92)))), ((int)(((byte)(80)))));
-            this.kryptonTextBoxPassword.StateCommon.Border.Color2 = System.Drawing.Color.Transparent;
-            this.kryptonTextBoxPassword.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
-            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
-            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
-            this.kryptonTextBoxPassword.StateCommon.Border.Rounding = 8;
-            this.kryptonTextBoxPassword.StateCommon.Border.Width = 2;
-            this.kryptonTextBoxPassword.StateCommon.Content.Font = new System.Drawing.Font("Poppins Medium", 12F);
-            this.kryptonTextBoxPassword.StateCommon.Content.Padding = new System.Windows.Forms.Padding(15);
-            this.kryptonTextBoxPassword.TabIndex = 3;
-            // 
-            // labelUserType
-            // 
-            this.labelUserType.AutoSize = true;
-            this.labelUserType.Font = new System.Drawing.Font("Poppins Medium", 12F);
-            this.labelUserType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(114)))), ((int)(((byte)(114)))));
-            this.labelUserType.Location = new System.Drawing.Point(263, 545);
-            this.labelUserType.Name = "labelUserType";
-            this.labelUserType.Size = new System.Drawing.Size(114, 36);
-            this.labelUserType.TabIndex = 6;
-            this.labelUserType.Text = "User Type";
-            // 
-            // kryptonComboBox1
-            // 
-            this.kryptonComboBox1.DropBackStyle = ComponentFactory.Krypton.Toolkit.PaletteBackStyle.ButtonStandalone;
-            this.kryptonComboBox1.DropButtonStyle = ComponentFactory.Krypton.Toolkit.ButtonStyle.Standalone;
-            this.kryptonComboBox1.DropDownWidth = 542;
-            this.kryptonComboBox1.Items.AddRange(new object[] {
-            "Tenant",
-            "Supermarket Staff"});
-            this.kryptonComboBox1.ItemStyle = ComponentFactory.Krypton.Toolkit.ButtonStyle.Standalone;
-            this.kryptonComboBox1.Location = new System.Drawing.Point(269, 595);
-            this.kryptonComboBox1.Name = "kryptonComboBox1";
-            this.kryptonComboBox1.Size = new System.Drawing.Size(542, 43);
-            this.kryptonComboBox1.StateCommon.ComboBox.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(92)))), ((int)(((byte)(80)))));
-            this.kryptonComboBox1.StateCommon.ComboBox.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
-            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
-            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
-            this.kryptonComboBox1.StateCommon.ComboBox.Border.Rounding = 8;
-            this.kryptonComboBox1.StateCommon.ComboBox.Border.Width = 2;
-            this.kryptonComboBox1.StateCommon.ComboBox.Content.Font = new System.Drawing.Font("Poppins Medium", 12F);
-            this.kryptonComboBox1.StateCommon.ComboBox.Content.Padding = new System.Windows.Forms.Padding(15);
-            this.kryptonComboBox1.TabIndex = 7;
-            // 
-            // kryptonButtonLogin
-            // 
-            this.kryptonButtonLogin.Location = new System.Drawing.Point(365, 715);
-            this.kryptonButtonLogin.Name = "kryptonButtonLogin";
-            this.kryptonButtonLogin.Size = new System.Drawing.Size(380, 89);
-            this.kryptonButtonLogin.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(92)))), ((int)(((byte)(80)))));
-            this.kryptonButtonLogin.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(92)))), ((int)(((byte)(80)))));
-            this.kryptonButtonLogin.StateCommon.Border.Color1 = System.Drawing.Color.Transparent;
-            this.kryptonButtonLogin.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
-            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
-            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
-            this.kryptonButtonLogin.StateCommon.Border.Rounding = 8;
-            this.kryptonButtonLogin.StateCommon.Content.ShortText.Color1 = System.Drawing.Color.White;
-            this.kryptonButtonLogin.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Poppins Medium", 16F);
-            this.kryptonButtonLogin.TabIndex = 8;
-            this.kryptonButtonLogin.Values.Text = "Login";
             // 
             // FormLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1422, 977);
-            this.Controls.Add(this.kryptonButtonLogin);
-            this.Controls.Add(this.kryptonComboBox1);
-            this.Controls.Add(this.labelUserType);
-            this.Controls.Add(this.labelPassword);
-            this.Controls.Add(this.kryptonTextBoxPassword);
-            this.Controls.Add(this.labelLogin);
-            this.Controls.Add(this.labelPhoneNumber);
-            this.Controls.Add(this.kryptonTextBoxPhoneNumber);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.ClientSize = new System.Drawing.Size(1955, 1466);
             this.Name = "FormLogin";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
-            ((System.ComponentModel.ISupportInitialize)(this.kryptonComboBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private ComponentFactory.Krypton.Toolkit.KryptonTextBox kryptonTextBoxPhoneNumber;
-        private System.Windows.Forms.Label labelPhoneNumber;
-        private System.Windows.Forms.Label labelLogin;
-        private System.Windows.Forms.Label labelPassword;
-        private ComponentFactory.Krypton.Toolkit.KryptonTextBox kryptonTextBoxPassword;
-        private System.Windows.Forms.Label labelUserType;
-        private ComponentFactory.Krypton.Toolkit.KryptonComboBox kryptonComboBox1;
-        private ComponentFactory.Krypton.Toolkit.KryptonButton kryptonButtonLogin;
     }
 }
