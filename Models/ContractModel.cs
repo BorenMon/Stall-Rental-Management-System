ecb043b6a044b500fb9632adf6394176c148be67
sokpheng001 <kimchansokpheng123@gmail.com>
2024-05-27 17:26:38 +0700

    Add: added class model for contract

---
+++ Models/ContractModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stall_Rental_Management_System.Models
+{
+    internal class ContractModel
+    {
+    }
+}
