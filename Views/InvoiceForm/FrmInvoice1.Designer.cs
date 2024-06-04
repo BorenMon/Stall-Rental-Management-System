5d2345c60366e3fead5ec6aa3e80cdb458db2b1f
setthi-pheap <157299176+setthi-pheap@users.noreply.github.com>
2024-06-04 14:50:04 +0700

    New invoice

---
+++ Views/InvoiceForm/FrmInvoice1.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Stall_Rental_Management_System.Views.InvoiceForm
+{
+    partial class FrmInvoice1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmInvoice1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 590);
+            this.Name = "FrmInvoice1";
+            this.Text = "FrmInvoice1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Views/InvoiceForm/FrmInvoice1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stall_Rental_Management_System.Views.InvoiceForm
+{
+    public partial class FrmInvoice1 : Form
+    {
+        public FrmInvoice1()
+        {
+            InitializeComponent();
+        }
+    }
+}
