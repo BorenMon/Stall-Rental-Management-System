58f25a17c501bc282c616d9b891d491a2f702b66
setthi-pheap11 <sattyphap@gmail.com>
2024-06-21 13:53:32 +0700

    MVP Invoice

---
+++ Repository/IInvoiceRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using Stall_Rental_Management_System.Models;
+
+namespace Stall_Rental_Management_System.Repositories
+{
+    public interface IInvoiceRepository
+    {
+        IEnumerable<InvoiceModel> GetAll();
+        void Add(InvoiceModel invoice);
+        void Update(InvoiceModel invoice);
+        void Delete(int invoiceID);
+        bool ContractIDExists(string contractID);
+    }
+}
